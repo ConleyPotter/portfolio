1b6c1362bd77893368869405a81b47199c5572b6
Conley Potter <conley.potter@gmail.com>
2019-07-15 08:56:59 -0500

    Finish up for now

--- src/components/DemoFooter.jsx
+++ src/components/DemoFooter.jsx
@@ -36,70 +36,29 @@ class DemoFooter extends React.Component {
         <footer className="footer">
           <Container>
             <Row>
-              <Col md="3">
+              <Col md="4">
                 <h1 className="title">BLK• React</h1>
               </Col>
-              <Col md="3" xs="6">
+              <Col md="4" xs="6">
                 <Nav>
                   <NavItem>
-                    <NavLink to="/index" tag={Link}>
-                      Home
-                    </NavLink>
+                    <a href="https://angel.co/conley-potter">
+                      AngelList
+                    </a>
                   </NavItem>
                   <NavItem>
-                    <NavLink to="/landing-page" tag={Link}>
-                      Landing
-                    </NavLink>
+                    <a href="https://www.linkedin.com/in/conley-potter/">
+                      LinkedIn
+                    </a>
                   </NavItem>
                   <NavItem>
-                    <NavLink to="/register-page" tag={Link}>
-                      Register
-                    </NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink to="/profile-page" tag={Link}>
-                      Profile
-                    </NavLink>
+                    <a href="https://github.com/ConleyPotter">
+                      GitHub
+                    </a>
                   </NavItem>
                 </Nav>
               </Col>
-              <Col md="3" xs="6">
-                <Nav>
-                  <NavItem>
-                    <NavLink
-                      href="https://creative-tim.com/contact-us?ref=blkdspr-footer"
-                      target="_blank"
-                    >
-                      Contact Us
-                    </NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink
-                      href="https://creative-tim.com/about-us?ref=blkdspr-footer"
-                      target="_blank"
-                    >
-                      About Us
-                    </NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink
-                      href="http://creative-tim.com/blog?ref=blkdspr-footer"
-                      target="_blank"
-                    >
-                      Blog
-                    </NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink
-                      href="https://opensource.org/licenses/MIT?ref=blkdspr-footer"
-                      target="_blank"
-                    >
-                      License
-                    </NavLink>
-                  </NavItem>
-                </Nav>
-              </Col>
-              <Col md="3">
+              <Col md="4">
                 <h3 className="title">Follow us:</h3>
                 <div className="btn-wrapper profile text-left">
                   <Button
@@ -109,34 +68,36 @@ class DemoFooter extends React.Component {
                     id="tooltip39661217"
                     target="_blank"
                   >
-                    <i className="fab fa-twitter" />
+                    <a href="https://github.com/ConleyPotter">
+                      <i className="fab fa-github" />
+                    </a>
                   </Button>
                   <UncontrolledTooltip delay={0} target="tooltip39661217">
-                    Follow us
+                    Follow Me
                   </UncontrolledTooltip>
                   <Button
                     className="btn-icon btn-neutral btn-round btn-simple ml-1"
                     color="default"
-                    href="https://www.facebook.com/creativetim"
+                    href="https://www.linkedin.com/in/conley-potter/"
                     id="tooltip206037619"
                     target="_blank"
                   >
-                    <i className="fab fa-facebook-square" />
+                    <i className="fab fa-linkedin" />
                   </Button>
                   <UncontrolledTooltip delay={0} target="tooltip206037619">
-                    Like us
+                    Connect with me!
                   </UncontrolledTooltip>
                   <Button
                     className="btn-icon btn-neutral btn-round btn-simple ml-1"
                     color="default"
-                    href="https://dribbble.com/creativetim"
+                    href="https://angel.co/conley-potter"
                     id="tooltip750293512"
                     target="_blank"
                   >
-                    <i className="fab fa-dribbble" />
+                    <i className="fab fa-angellist" />
                   </Button>
                   <UncontrolledTooltip delay={0} target="tooltip750293512">
-                    Follow us
+                    Connect with me!
                   </UncontrolledTooltip>
                 </div>
               </Col>
